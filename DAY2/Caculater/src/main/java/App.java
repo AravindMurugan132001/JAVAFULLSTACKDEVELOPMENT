b3473beda65c0fabb490ba4aa2320715d049f962
AravindMurugan132001 <Administrator@DELL-JLGFFL3.AD.NSEIT.COM>
2022-08-12 18:04:31 +0530

    2nd Commit

---
+++ DAY2/Caculater/src/main/java/App.java
@@ -0,0 +1,10 @@
+public class App
+{
+    public static void main(String[] args)
+    {
+        Calculation calculation = new Calculation(10,20,30,60,70);
+        // calculation.add(10,20);
+    }
+
+
+}
---
+++ DAY2/Caculater/src/main/java/Calculation.java
@@ -0,0 +1,9 @@
+public class Calculation
+{
+    public Calculation(int a,int b,int c,int d,int e)
+    {
+        int s=a+b+c+d+e;
+        System.out.println("Constructor with 5 Arguments : "+ s);
+    }
+
+}
