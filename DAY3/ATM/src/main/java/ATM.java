c94fcf91162e82e3a6ab7f7c32811d7629b34887
AravindMurugan132001 <Administrator@DELL-JLGFFL3.AD.NSEIT.COM>
2022-08-16 17:56:07 +0530

    ATM ongoing Project

--- DAY3/ATM/src/main/java/ATM.java
+++ DAY3/ATM/src/main/java/ATM.java
@@ -1,3 +1,37 @@
+import java.awt.*;
+import java.util.Scanner;
+
 public class ATM {
+    public static void main(String[] args) {
+        long initialBalance = 100_000;
+        Menu menu = new Menu();
+        Scanner scanner = new Scanner(System.in);
+        int Choice;
+        do {
+            menu.display();
+            System.out.print("Enter Your Choice : ");
+            Choice = scanner.nextInt();
+           // System.out.println(Choice);
+            switch (Choice){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    System.out.println("***Thank You For Visiting***");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("**Please Enter Valid Input**");
+                    break;
+            }
+
+        }
+        while (Choice > 0 && Choice < 5);
+        System.out.println("***Thank You For Visiting***");
+
+    }
 
 }
--- DAY3/ATM/src/main/java/Menu.java
+++ DAY3/ATM/src/main/java/Menu.java
@@ -1,6 +1,10 @@
 public class Menu {
-    public void Display() {
+    public void display() {
         System.out.println("Welcome To ATM");
-        System.out.println("");
+        System.out.println("Choose 1. for Withdrawal");
+        System.out.println("Choose 2. for Deposit");
+        System.out.println("Choose 3. for View Balance");
+        System.out.println("Choose 4. for Exit");
     }
+
 }
